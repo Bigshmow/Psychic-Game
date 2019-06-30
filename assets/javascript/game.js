ccf293d371a2ef9d20bde573386d5b833a3ff11d
Bigshmow <dstewart88@gmail.com>
2019-06-30 14:12:08 -0700

    added psuedo code to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// user starts with 9 guesses, 0 wins, 0 losses
+// pc will 'think' of a random letter
+    // the letter becomes stored
+// user will guess a letter
+// pc checks letter
+    // if the letter is correct - user wins
+    // if the letter is incorrect - user guesses decrease by 1
+//if user guess = 0 - game over
