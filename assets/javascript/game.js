36f555edc952aed6962a2cf5d1e2ff139f8430f5
Bigshmow <dstewart88@gmail.com>
2019-07-01 09:08:11 -0700

    added onkeyup and if for wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,27 @@ var losses = 0;
 var guesses = 9;
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","v"];
 
+
 function pcthink() {
     var rnum = Math.floor(Math.random() * letters.length);
     var rletter = letters[rnum];
     document.getElementById("pcguess").innerHTML = rletter;
+    
+
+document.onkeyup = function(event) {
+    var userguess = event.key.toLowerCase();
+
+    if (userguess === rletter) {
+        alert("working");
+        wins++;
+        console.log(wins);
     }
+    }
+}
+
 
 pcthink();
 console.log(letters);
-document.getElementById("pcguess").innerHTML = rletter;
+console.log(wins);
 
 
