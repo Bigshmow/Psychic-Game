566d80e8972a3ed07e9690e5b6038a018d64fdd9
Bigshmow <dstewart88@gmail.com>
2019-07-01 10:41:22 -0700

    fine tuned ifguess and else{if guess} added alert to guessing correctly and added innerhtml for your guesses so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // alert("js link worked!!");
-var wins = 0;
+var wins = 1;
 var losses = 0;
-var guesses = 9;
+var guesses = 8;
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","v"];
 
 
@@ -9,20 +9,40 @@ function pcthink() {
     var rnum = Math.floor(Math.random() * letters.length);
     var rletter = letters[rnum];
     document.getElementById("pcguess").innerHTML = rletter;
+function resetloss() {
+    losses++;
+    guesses = 9;
+}
     
 
 document.onkeyup = function(event) {
     var userguess = event.key.toLowerCase();
 
     if (userguess === rletter) {
-        wins++;
         document.getElementById("wins").innerHTML = "Wins: " + wins;
-        console.log(wins);
+        console.log("the wins " + wins);
+        if (guesses < 2) {
+            resetloss();
+        }
+        else {
+            wins++;
+            alert("You Guessed Correctly");
+            pcthink();
+        }
     }
     else {
-        losses++;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
-        console.log(losses);
+        document.getElementById("guessesleft").innerHTML = "Guesses Left: " + guesses;
+        document.getElementById("lastguess").innerHTML = "Your Guesses So Far: " + userguess;
+        console.log("the losses" + losses);
+        console.log("the guesses" + guesses);
+        if (guesses < 2) {
+            resetloss();
+        }
+        else {
+            guesses--;
+            pcthink();
+        }
     }
     }
 }
